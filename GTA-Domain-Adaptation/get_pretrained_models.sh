0a5ab0de60f356ab877ea1f3e46b2c3aadd43941
Krishna14 <krishna1994sree@gmail.com>
2019-04-08 08:12:26 -0700

    Final commit

---
+++ GTA-Domain-Adaptation/get_pretrained_models.sh
@@ -0,0 +1,11 @@
+mkdir saved_models
+cd saved_models
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_semi_generator_a.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_semi_generator_a.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_semi_generator_b.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_semi_generator_b.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_un_generator_a.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_un_generator_a.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_un_generator_b.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_un_generator_b.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_semantic_generator_b.pth
